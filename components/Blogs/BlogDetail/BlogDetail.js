97f8552eb35a7209663a41b27f7ff622525b14bb
ying <ying285@hotmail.com>
2021-12-28 15:39:41 +0100

    Changed Image elem to img.

--- components/Blogs/BlogDetail/BlogDetail.js
+++ components/Blogs/BlogDetail/BlogDetail.js
@@ -1,11 +1,10 @@
 import classes from "./BlogDetail.module.css";
-import Image from "next/image";
 
 const BlogDetail = (props) => {
   return (
     <section className={classes.detail}>
       <div className={classes.imageDetail}>
-        <Image src={props.image} alt={props.title} />
+        <img src={props.image} alt={props.title} />
       </div>
 
       <h1>{props.title}</h1>
